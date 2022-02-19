4f6e0f94d90f674db2fe85a114acdfe125b8d207
David Brandao <davfb.ferreira@gmail.com>
2022-02-19 14:43:25 -0300

    Create and Read JSON files

--- src/aula04/Aula04.java
+++ src/aula04/Aula04.java
@@ -11,31 +11,32 @@ public class Aula04 {
 
 	public static void main(String[] args) {
 		
-//		//Revisão de Estrutura de dados
-//		ReviewEstruturasDados estruturas = new ReviewEstruturasDados();
-//		
-//		estruturas.TrabalhandoFilas();
-//		estruturas.TrabalhandoListas();
-//		estruturas.TrabalhandoPilhas();
+		//Revisão de Estrutura de dados
+		ReviewEstruturasDados estruturas = new ReviewEstruturasDados();
+		
+		estruturas.TrabalhandoFilas();
+		estruturas.TrabalhandoListas();
+		estruturas.TrabalhandoPilhas();
 		
 		
-//		ExercisesList05 lista05 = new ExercisesList05();
-//		//Solução utilizando Regex para mapear dados
-//		JOptionPane.showMessageDialog(null, lista05.ArmazenarLetrasFilaRegex(lista05.value01+lista05.value02));
-//		JOptionPane.showMessageDialog(null, lista05.ArmazenarNumerosFilaRegex(lista05.value01+lista05.value02));
-//		//Solução utilizando Collections para mapear dados 
-//		JOptionPane.showMessageDialog(null, lista05.RetornarLetras(lista05.value01+lista05.value02));
-//		JOptionPane.showMessageDialog(null, lista05.RetornarNumeros(lista05.value01+lista05.value02));
+		ExercisesList05 lista05 = new ExercisesList05();
+		//Solução utilizando Regex para mapear dados
+		JOptionPane.showMessageDialog(null, lista05.ArmazenarLetrasFilaRegex(lista05.value01+lista05.value02));
+		JOptionPane.showMessageDialog(null, lista05.ArmazenarNumerosFilaRegex(lista05.value01+lista05.value02));
+		//Solução utilizando Collections para mapear dados 
+		JOptionPane.showMessageDialog(null, lista05.RetornarLetras(lista05.value01+lista05.value02));
+		JOptionPane.showMessageDialog(null, lista05.RetornarNumeros(lista05.value01+lista05.value02));
 
 		
-//		//Trabalhando com arquivos - TXT			
-//		ReviewWorkingFiles arquivos = new ReviewWorkingFiles();
-//		arquivos.ExcrevendoArquivos();
-//		arquivos.LendoArquivos();
+		//Trabalhando com arquivos - TXT			
+		ReviewWorkingFiles arquivos = new ReviewWorkingFiles();
+		arquivos.ExcrevendoArquivos();
+		arquivos.LendoArquivos();
 		
 		//Trabalhando com arquivos - JSON	
 		ReviewJson myJson = new ReviewJson();
 		myJson.CriarJson();
+		myJson.LerJson();
 		
 		
 	}
--- src/aula04/ReviewJson.java
+++ src/aula04/ReviewJson.java
@@ -1,9 +1,11 @@
 package aula04;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class ReviewJson {
 	
@@ -26,6 +28,33 @@ public class ReviewJson {
 			FileWriter arquivoJson = new FileWriter("outputFiles/exemplo.json", false);
 			arquivoJson.write(nomes.toJSONString());
 			arquivoJson.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void LerJson() {
+		JSONParser parser = new JSONParser();
+		
+		try {
+			FileReader file = new FileReader("outputFiles/exemplo.json");
+			JSONObject fileJson= (JSONObject) parser.parse(file);
+			
+			String ultimoNome = (String) fileJson.get("Ultimo Nome");
+			System.out.println(ultimoNome);
+			
+			String primeiroNome = (String) fileJson.get("Primeiro Nome");
+			System.out.println(primeiroNome);
+			
+			JSONArray mensagens = (JSONArray) fileJson.get("Mensagens");
+			for(Object m : mensagens) {
+				System.out.println(m);
+			}
+			
+			
+			file.close();
+			
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
