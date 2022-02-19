f770f1245321f73ea323cce87129fb57f3427ad1
David Brandao <davfb.ferreira@gmail.com>
2022-02-18 21:15:52 -0300

    Adicionando objeto gato na main class

--- src/aula03/Aula03.java
+++ src/aula03/Aula03.java
@@ -7,24 +7,26 @@ public class Aula03 {
 	public static void main(String[] args) {
 	
 			ReviewCachorro toto = new ReviewCachorro("azul", 10, "cheetos", 0.6, "relogio", "marrom");
-			
-//			// Informando valores para os atributos
-//			toto.setAcessorios("Relógio");
-//			toto.setAltura(1.75);
-//			toto.setCor("Amalero");
-//			toto.setPeso(3.5);
-//			toto.setRaca("SRD");
-		
+
 			//mostrar dados do totó
-			JOptionPane.showMessageDialog(null, "Acessorios: " + toto.getAcessorios() + "\nAltura: " + toto.getAltura() + 
+			JOptionPane.showMessageDialog(null, "Cachorro: "+"\nAcessorios: " + toto.getAcessorios() + "\nAltura: " + toto.getAltura() + 
 												"\nCor: " + toto.getCor() + "\nPeso: " + toto.getPeso() + "\nRaca: " + toto.getRaca());
 			
 			//Chamando os métodos
-			toto.Caminhar();
-			toto.Correr();
-			toto.Escrever();
-			toto.Latir();
-			toto.VerificarHoras();			
+//			toto.Caminhar();
+//			toto.Correr();
+//			toto.Escrever();
+//			toto.Latir();
+//			toto.VerificarHoras();		
+//			
+			ReviewGato gato = new ReviewGato("rosa", 5, "miau", 0.4, "agressivo", "marrom");
+			
+			JOptionPane.showMessageDialog(null, "Gato: "+ "\nAltura: " + gato.getAltura() + "\nCor: " + gato.getCor() + "\nPeso: " + gato.getPeso() +
+											   "\nRaca: " + gato.getRaca()+ "\nTemperamento: " + gato.getTemperamento() + "\nCor do Rabo" + gato.getCorRabo());
+			
+//			gato.Caminhar();
+//			gato.Correr();
+//		
 
 	}
 
